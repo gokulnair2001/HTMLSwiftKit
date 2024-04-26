fe0e8b67b5abf2dcce02ab82f47c582de00d2ec3
Gokul Nair <gokul.getpowerplay.in>
2024-04-26 12:44:06 +0530

    List item style modifier

--- Sources/HTMLSwiftKit/Elements/HTML/ListItem.swift
+++ Sources/HTMLSwiftKit/Elements/HTML/ListItem.swift
@@ -11,9 +11,17 @@ import Foundation
 /// ListItem modifier is used to represent an item in a list.
 public struct ListItem: HTMLBlock {
     
-    public var content: String
+    public var content: String = ""
     
     public init(_ text: String) {
         self.content = "<li> \(text) </li>"
+    }
+    
+    public init(@CSSModifierBuilder _ style: () -> [CSSModifier], _ text: String) {
+        self.content = "<li style='\(generateCSSInlineCode(cssModifiers: style()))'> \(text) </li>"
+    }
+    
+    public init(style: CSSModifier, text: String) {
+        self.content = "<li style='\(style.content))'> \(text) </li>"
     }
 }
